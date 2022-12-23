45ca69f506ed29feb4e3173e46e2dacebbebf040
Josh S <99520694+steawth@users.noreply.github.com>
2022-12-22 21:18:18 -0600

    Finalized Mod Commit

---
+++ Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using MelonLoader;
+using HarmonyLib;
+using FadeFix;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using RTG;
+using PlayerNameplate = ABI_RC.Core.Player.PlayerNameplate;
+using RefFlags = System.Reflection.BindingFlags;
+
+namespace FadeFix
+{
+    
+    public static class BuildInfo
+    {
+        public const string Name = "NameplateFadeFix"; // Name of the Mod.  (MUST BE SET)
+        public const string Description = "Mod for fixing fade on nameplate images"; // Description for the Mod.  (Set as null if none)
+        public const string Author = "Stealth"; // Author of the Mod.  (MUST BE SET)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class ModMain: MelonMod
+    {
+        private const string SettingsCategory = "NameplateFadeFix";
+        private const string SettingEnableMod = "Enabled";
+
+        public static MelonPreferences_Entry<bool> ourEnable;
+
+        public static MelonLogger.Instance Logger;
+        public static ModMain Instance { get; set; }
+
+        public override void OnApplicationStart() // Runs after Game Initialization.
+        {
+            Logger = LoggerInstance;
+
+            var category = MelonPreferences.CreateCategory(SettingsCategory, "Nameplate Fade Fix");
+            ourEnable = category.CreateEntry(SettingEnableMod, true, "Enabled");
+
+            Instance = this;
+            Logger.Msg("Fixing Color Fade on Nameplate Images");
+        }
+    }
+}
+
+
+[HarmonyPatch(typeof(PlayerNameplate))]
+class NameplatePatches
+{
+    [HarmonyPatch(nameof(PlayerNameplate.UpdateNamePlate))]
+    [HarmonyPostfix]
+    static void UpdateNameplate(PlayerNameplate __instance)
+    {
+        if (ModMain.ourEnable.Value)
+        {
+            if (__instance.objectMaskSlave != null)
+                try
+                {
+                    var userImgMask = __instance.objectMaskSlave.transform.Find("UserImageMask")?.GetComponent<Image>();
+                    if (userImgMask != null)
+                    {
+                        userImgMask.color = Color.black;
+                    }
+                    else
+                    {
+                        ModMain.Logger.Msg("Error Fixing Nameplate Fade: UserImageMask is null (and bono is a nerd)");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Debug.Log(e);
+                }
+        }
+    }
+        
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+using MelonLoader;
+using FadeFix;
+
+
+[assembly: AssemblyTitle(FadeFix.BuildInfo.Description)]
+[assembly: AssemblyDescription(FadeFix.BuildInfo.Description)]
+[assembly: AssemblyCompany(FadeFix.BuildInfo.Company)]
+[assembly: AssemblyProduct(FadeFix.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + FadeFix.BuildInfo.Author)]
+[assembly: AssemblyTrademark(FadeFix.BuildInfo.Company)]
+[assembly: AssemblyVersion(FadeFix.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(FadeFix.BuildInfo.Version)]
+[assembly: MelonInfo(typeof(ModMain), FadeFix.BuildInfo.Name, FadeFix.BuildInfo.Version, FadeFix.BuildInfo.Author, FadeFix.BuildInfo.DownloadLink)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonColor(ConsoleColor.Magenta)]
+
+// Create and Setup a MelonGame Attribute to mark a Melon as Universal or Compatible with specific Games.
+// If no MelonGame Attribute is found or any of the Values for any MelonGame Attribute on the Melon is null or empty it will be assumed the Melon is Universal.
+// Values for MelonGame Attribute can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
